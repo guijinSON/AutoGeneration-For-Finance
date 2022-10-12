3960e65ae65bddab2901805929c64d50a07de718
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-10-12 09:59:08 +0900

    Update emb.py

--- hallucination/emb.py
+++ hallucination/emb.py
@@ -6,7 +6,7 @@ def mean_pooling(model_output, attention_mask):
     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
     return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
-def emb_sentence(sentence, model):
+def emb_sentence(sentence, model, tokenizer):
     encoded_input = tokenizer(sentence, padding=True, truncation=True, return_tensors='pt')
     model_output = model(**encoded_input)
     sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
