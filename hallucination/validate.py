3d6480a34da60c10504616cbced4823b29863f12
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-10-14 09:22:00 +0900

    Create validate.py

---
+++ hallucination/validate.py
@@ -0,0 +1,96 @@
+import spacy
+import re
+import country_converter as coco
+import torch.nn as nn
+from emb import emb_sentence
+
+# df = pd.read_csv('')
+
+def single_epoch_validate(df,):
+  cos = nn.CosineSimilarity(dim=1, eps=1e-6)
+  NER = spacy.load('en_core_web_sm')
+  
+  for row in df.iterrows():
+    row = row[1]
+    src = row['seg_text_origin']
+    tgt1 = row['seg_text']
+    tgt2 = row['seg_text_clean']
+
+    """
+    semantic comparison
+    """
+    src_emb = emb_sentence(src,model, tokenizer)
+
+    if (len(tgt1)//len(tgt2)) > 1.5:
+        n = len(tgt1)//len(tgt2)
+        seqlen = len(tgt1)//n
+        sliced_tgt_emb = [cos(src_emb, emb_sentence(tgt1[i*seqlen:(i+1)*seqlen],model,tokenizer)).item() \
+                          for i in range(len(tgt1)//seqlen)]
+        an = sum(sliced_tgt_emb)/n
+    
+    else:
+        tgt1_emb = emb_sentence(tgt1,model, tokenizer)
+        an = cos(src_emb,tgt1_emb).item()
+    
+    tgt2_emb = emb_sentence(tgt2,model, tokenizer)
+    
+    bn = cos(src_emb,tgt2_emb).item()
+
+    """
+    number comparison 
+    """
+
+    src_num = re.findall("\d+", src)
+    tgt1_num = re.findall("\d+",tgt1)
+    tgt2_num = re.findall("\d+", tgt2)
+
+    n_an = [n for n in tgt1_num if n not in src_num]
+    n_bn = [n for n in tgt2_num if n not in src_num]
+
+    """
+    entity comparison #1
+    """
+
+    src_entity  = [(_.text,_.label_) for _ in NER(src).ents]
+    tgt1_entity = [(_.text,_.label_) for _ in NER(tgt1).ents]
+    tgt2_entity = [(_.text,_.label_) for _ in NER(tgt2).ents]
+    
+    """
+    PERSON Entity
+    """
+    src_entity_p  = [_[0] for _ in src_entity if _[1] == 'PERSON']
+    tgt1_entity_p = [_[0] for _ in tgt1_entity if _[1] == 'PERSON']
+    tgt2_entity_p = [_[0] for _ in tgt2_entity if _[1] == 'PERSON']
+
+    ep_an = [n for n in tgt1_entity_p if n not in src_entity_p]
+    ep_bn = [n for n in tgt2_entity_p if n not in src_entity_p]
+
+    """
+    GPE Entity
+    """
+
+    src_entity_g  = coco.convert(names=[_[0] for _ in src_entity if _[1] == 'GPE'], to='name_short')
+    src_entity_g = [src_entity_g] if isinstance(src_entity_g, str) else src_entity_g
+
+    tgt1_entity_g = coco.convert(names=[_[0] for _ in tgt1_entity if _[1] == 'GPE'], to='name_short')
+    tgt1_entity_g = [tgt1_entity_g] if isinstance(tgt1_entity_g, str) else tgt1_entity_g
+
+    tgt2_entity_g = coco.convert(names=[_[0] for _ in tgt2_entity if _[1] == 'GPE'], to='name_short')
+    tgt2_entity_g = [tgt2_entity_g] if isinstance(tgt2_entity_g, str) else tgt2_entity_g
+
+    eg_an = [n for n in tgt1_entity_g if n not in src_entity_g]
+    eg_bn = [n for n in tgt2_entity_g if n not in src_entity_g]
+
+
+    e_an = len(n_an) + len(ep_an) + len(eg_an)
+    e_bn = len(n_bn) + len(ep_bn) + len(eg_bn)
+
+    if e_an > e_bn:
+        output.append(1)
+    else:
+        if an < bn:
+            output.append(1)
+        else:
+            output.append(0)
+            
+return sum(output)/len(output)
