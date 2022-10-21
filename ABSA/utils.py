c384f67fe4a7c1386cb17cbf17fa415281f2b2bc
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-10-21 11:06:20 +0900

    Update utils.py

--- ABSA/utils.py
+++ ABSA/utils.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoModelForSeq2SeqLM
 
 def get_model_tokenizer(MODEL_PATH):
     tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
@@ -10,12 +10,9 @@ def get_model_tokenizer(MODEL_PATH):
     return model, tokenizer
 
 
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-
 def get_T5_tokenizer(MODEL_PATH='t5-large'):
     tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
-    model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
+    model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_PATH)
 
     num_added_toks = tokenizer.add_tokens(['[TGT]'])
     num_added_toks = tokenizer.add_tokens(['POSITIVE'])
