fe91bc2cb591f617704feaa57b0d06e355009904
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2023-09-30 21:29:33 +0900

    Update train.py

--- trainer/train.py
+++ trainer/train.py
@@ -1,50 +1,71 @@
-import random
+import yaml
+import os
+import torch
+from transformers import TrainerCallback, TrainingArguments, ProgressCallback, AutoModelForCausalLM
+from datasets import load_dataset
+import argparse
+from trl import SFTTrainer
+from src import prepare_model_for_optimized_training,count_trainable_params
 
-def prepare_model_for_optimized_training(model, model_name, freezing_strategy, freeze_ratio=0.5, unfreeze_layers=8):
-    
-    if freezing_strategy not in ["first", "last", "intermediate", "alternating", "random"]:
-        raise ValueError(f"{freezing_strategy} not defined. Choose from 'first', 'last', 'intermediate', 'alternating', or 'random'.")
+# ... [all the class definitions from your code]
 
-    if model_name not in ["polyglot", "xglm"]:
-        warnings.warn(f"{model_name} not expected. Models other than 'polyglot' and 'xglm' may raise errors.")
+def load_config(yaml_file_path):
+    with open(yaml_file_path, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
 
-    layer_name = "gpt_neox.layers" if model_name == 'polyglot' else "model.layers"
-    
-    total_layers = max([int(name.split('.')[2]) for name, _ in model.named_parameters() if name.startswith(layer_name)])
-    
-    def get_freezing_mask(strategy):
-        """Helper function to get a boolean mask indicating which layers should be frozen."""
-        if strategy == "first":
-            return [i < (unfreeze_layers * freeze_ratio) for i in range(total_layers)]
-        elif strategy == "last":
-            return [(total_layers - i) < (unfreeze_layers * freeze_ratio) for i in range(total_layers)]
-        elif strategy == "intermediate":
-            return [i % unfreeze_layers == 0 for i in range(total_layers)]
-        elif strategy == "alternating":
-            return [i % 2 == 0 for i in range(total_layers)]
-        elif strategy == "random":
-            return random.sample([True, False] * (total_layers // 2), total_layers)[:int(total_layers * freeze_ratio)]
-        else:
-            raise ValueError(f"Unknown freezing strategy: {strategy}")
+def main(config_path):
+    config = load_config(config_path)
 
-    freezing_mask = get_freezing_mask(freezing_strategy)
-    
-    for idx, (name, param) in enumerate(model.named_parameters()):
-        if name.startswith(layer_name):
-            layerN = int(name.split('.')[2])
-            param.requires_grad = freezing_mask[layerN]
-        else:
-            param.requires_grad = False
+    os.environ["WANDB_API_KEY"] = config['general']['wandb_api_key']
+    os.environ["WANDB_PROJECT"] = config['general']['wandb_project']
 
-    if param.ndim == 1 and "layer_norm" in name:  # cast the small parameters (e.g. layernorm) to fp32 for stability
-        param.data = param.data.to(torch.float16)
+    data_path = config['general']['data_path']
+    model_name = config['general']['model_name']
+    freezing_strategy = config['general']['freezing_strategy']
+    base_model_path = config['general']['base_model_path']
 
-    model.gradient_checkpointing_enable()  # reduce number of stored activations
-    model.get_input_embeddings().register_forward_hook(make_inputs_require_grad)
+    dataset = load_dataset(data_path, split='train')
 
-    if model_name == 'polyglot':
-        model.embed_out = CastOutputToFloat(model.embed_out)
-    else:  
-        model.lm_head =  CastOutputToFloat(model.lm_head)
-    
-    return model
+    model = AutoModelForCausalLM.from_pretrained(base_model_path, device_map='auto')
+
+    model = prepare_model_for_optimized_training(
+        model, 
+        model_name=model_name,
+        freezing_strategy=freezing_strategy,
+        freeze_ratio=config['freeze_parameters']['freeze_ratio']
+    )
+
+    count_trainable_params(model)
+    model.config.use_cache = False 
+
+    training_args = TrainingArguments(
+        output_dir= config['general']['ckpt']
+        report_to="wandb",
+        overwrite_output_dir=True,
+        num_train_epochs=config['training']['num_train_epochs'],
+        per_device_train_batch_size=config['training']['per_device_train_batch_size'],
+        gradient_accumulation_steps=config['training']['gradient_accumulation_steps'],
+        logging_steps=config['training']['logging_steps'],
+        save_strategy=config['training']['save_strategy'],
+        save_steps=config['training']['save_steps']
+    )
+
+    trainer = SFTTrainer(
+        model,
+        train_dataset=dataset,
+        dataset_text_field=config['training']['dataset_text_field'],
+        packing=config['training']['packing'],
+        max_seq_length=config['training']['max_seq_length'],
+        args=training_args
+    )
+
+    trainer.train()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Train a model using configurations from a YAML file.')
+    parser.add_argument('config', type=str, help='Path to the YAML configuration file.')
+
+    args = parser.parse_args()
+
+    main(args.config)
